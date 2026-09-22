178351333965e1bf31bc58d3ca065c6fe73bdf63
Carla22-dev <cf97451@gmail.com>
2026-09-22 11:33:07 +0100

    Version initial

---
+++ cipher.py
@@ -0,0 +1,23 @@
+
+def encode(message, rails):
+    
+    matrix=[
+        [] # o que quero fazer 
+        for _ in range( rails) #para este numero de rails
+    ]
+    current_rail=0
+    
+    for cipher in message:
+        matrix[current_rail].append(cipher)
+        if current_rail== rails-1:
+            direction=-1
+        elif current_rail==0:
+            direction=1
+        current_rail+=direction
+    matrix =''.join(''.join(row) for row in matrix)
+    return matrix
+
+print("The matrix is ", encode("WEAREDISCOVEREDFLEEATONCE", 3))
+
+
+def decode(encoded_message, rails):
