9b8e1bba948395635ad26dd6a55b58e25c364a96
Carla22-dev <cf97451@gmail.com>
2026-09-23 11:54:25 +0100

    WITH SMALLEST AND LARGEST BU NOT WORKING YET

--- palindrome_products.py
+++ palindrome_products.py
@@ -8,18 +8,29 @@ def reversed_digits(n):
 
 def palindrome (min_factor , max_factor):
     result_product=[]
+    result_number_multipl=[]
+    position=0
+    dict_factor={}
+    already_exist=False
 
-    for i in range (1,max_factor+1):
-        #print(i)
-        for j in range (1,max_factor+1):
+    for i in range (min_factor,max_factor+1):
+        for j in range (min_factor,max_factor+1):
             product_btw_value=i*j
-            #print(product_btw_value)
+            new_pair=sorted([i,j])
             if product_btw_value==reversed_digits(product_btw_value):
-                print(product_btw_value)
+                if product_btw_value not in dict_factor:
+                    dict_factor[product_btw_value]=[]
+                already_exist=False
+                for pair in dict_factor[product_btw_value]:
+                    if sorted(pair)==new_pair:
+                        already_exist=True
+                        break
+                if not already_exist:
+                    dict_factor[product_btw_value].append([i,j])
+                
                 if product_btw_value not in result_product:
                     result_product.append(product_btw_value)
-            continue
-    return result_product     
+    return dict_factor     
 
 
 def largest(min_factor, max_factor):
@@ -33,7 +44,10 @@ def largest(min_factor, max_factor):
              Iterable should contain both factors of the palindrome in an arbitrary order.
     """
     result_palindrome=palindrome(min_factor,max_factor)
-    return max(result_palindrome)
+    max_namber=max(result_palindrome)
+    tup=()
+    tup=(max_namber,list(result_palindrome[max_namber]))
+    return tup
 
 def smallest(min_factor, max_factor):
     
@@ -46,7 +60,11 @@ def smallest(min_factor, max_factor):
     Iterable should contain both factors of the palindrome in an arbitrary order.
     """
     result_palindrome=palindrome(min_factor,max_factor)
-    return min(result_palindrome)
+    min_number=min(result_palindrome)
+    tup=()
+    tup=(min_number,list(result_palindrome[min_number]))
+    return tup
+    
 
 
 print(f"value of list{palindrome(10,99)} small {smallest(10,99)}  largest {largest(10,99)}:", )
