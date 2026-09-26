24d3b2587d9b79001ff3bfa5aeb58c3f943f8209
Carla22-dev <cf97451@gmail.com>
2026-09-26 05:01:01 +0100

    version intial

---
+++ recipe.py
@@ -0,0 +1,13 @@
+def recite(start_verse, end_verse):
+
+    dict_animal={"spider": ["It wriggled and jiggled and tickled inside her.","She swallowed the spider to catch the fly."],
+                 "bird":["How absurd to swallow a bird!"," She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her."],
+                 "cat":["Imagine that, to swallow a cat!"," She swallowed the cat to catch the bird."],
+                 "dog":["What a hog, to swallow a dog!"," She swallowed the dog to catch the cat."],
+                 "goat":["Just opened her throat and swallowed a goat!"," She swallowed the goat to catch the dog."],
+                 "cow":["I don't know how she swallowed a cow!"," She swallowed the cow to catch the goat."],
+                 "horse":["She's dead, of course"]
+                }
+    list_animal=["fly","spider","bird","cat","dog","goat","cow","house"]
+    for i in list_animal:
+        
